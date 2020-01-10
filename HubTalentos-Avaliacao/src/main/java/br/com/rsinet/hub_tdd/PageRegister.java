50b375d7f000ec4c5e1e7df3c0ae56feecaaccfb
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-10 11:11:16 -0300

    Foi feito o mapeamento dos elemetnos
    Foi mapeado os elementos da página e criado metodos de Setters para os
    campos

--- HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/PageRegister.java
+++ HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/PageRegister.java
@@ -1,49 +1,120 @@
 package br.com.rsinet.hub_tdd;
 
-import java.util.concurrent.TimeUnit;
- 
-import org.openqa.selenium.WebDriver;
- 
-import org.openqa.selenium.WebElement;
- 
-import org.openqa.selenium.firefox.FirefoxDriver;
- 
-import org.openqa.selenium.support.FindBy;
- 
-import org.openqa.selenium.support.How;
- 
-import org.openqa.selenium.support.PageFactory;
-
-import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.Select;
 
+public class PageRegister {
 
-public class PageRegister{
+	final WebDriver driver;
+	/////////////Elements: Account Details////////////////
+	@FindBy(how = How.NAME, using = "usernameRegisterPage")
 
-	private WebDriver driver;
+	public WebElement elementUserName;
+	
+	@FindBy(how = How.NAME, using = "emailRegisterPage")
+
+	public WebElement elementEmail;
 	
 	
-//
-//	public PageRegister(WebDriver driver) {
-//		this.driver = driver;
-//	}
-//	
-//	public PageRegister setUsername(String username){
-//		driver.findElement
-//		(By.xpath("/html/body/div[3]/section/article/sec-form/div[1]/div[2]/div/div[1]/div[1]/sec-view[1]/div/input"))
-//			.sendKeys(username);
-//		return this;
-//	}
+	@FindBy(how = How.NAME, using = "passwordRegisterPage")
+
+	public WebElement elementPassword;
+	@FindBy(how = How.NAME, using = "confirm_passwordRegisterPage")
+
+	public WebElement elementConfirmPassword;
 	
+	/////////////Elements: Personal Details////////////////
+	@FindBy(how = How.NAME, using = "first_nameRegisterPage")
+
+	public WebElement elementFirstName;
 	
-	 
-	 @FindBy(how = How.NAME, using = "submit")
-	 
-	 public static WebElement txtbx_Password;
-	 
+	@FindBy(how = How.NAME, using = "last_nameRegisterPage")
+
+	public WebElement elementLastName;
 	
+	@FindBy(how = How.NAME, using = "phone_numberRegisterPage")
+
+	public WebElement elementPhoneNumber;
+/////////////Elements: Adress////////////////
+	@FindBy(how = How.NAME, using = "countryListboxRegisterPage")
+
+	public WebElement elementCountry;
+	
+	@FindBy(how = How.NAME, using = "cityRegisterPage")
+
+	public WebElement elementCity;
+	
+	@FindBy(how = How.NAME, using = "addressRegisterPage")
+
+	public WebElement elementAdress;
+	
+	@FindBy(how = How.NAME, using = "state_/_province_/_regionRegisterPage")
+
+	public WebElement elementState;
+	
+	@FindBy(how = How.NAME, using = "postal_codeRegisterPage")
+
+	public WebElement elementPostalCode;
+
+	
+	////////////////////////////////////////////////////////
+	public PageRegister(WebDriver driver) {
+
+		this.driver = driver;
+	}
+	
+/**
+ * Methods Account Details
+ */
+	
+public void setUserName(String username){
+	elementUserName.sendKeys(username);}
+
+public void setEmai(String email){
+	elementEmail.sendKeys(email);}
+
+public void setPassword(String password){
+	elementPassword.sendKeys(password);}
+
+public void setConfirmPassword(String password){
+	elementConfirmPassword.sendKeys(password);}
+
+/**
+ * Methods Personal Details
+ * */
+public void setFirstName(String fistName){
+	elementFirstName.sendKeys(fistName);}
+
+public void setLastName(String lastName){
+	elementLastName.sendKeys(lastName);}
+
+public void setPhoneNumber(String phoneNumber){
+	elementPhoneNumber.sendKeys(phoneNumber);}
 
 
+/**
+ * Methods Account Details
+ */
+public void selectCountry(String valor) {
+	WebElement element = driver.findElement(By.name("countryListboxRegisterPage"));
+		Select combo = new Select(element);
+		combo.selectByVisibleText(valor);
+		}
+public void setCity(String city){
+	elementCity.sendKeys(city);}
+		
+public void setAdress(String adress){
+	elementAdress.sendKeys(adress);}
+		
+public void setState(String state){
+	elementState.sendKeys(state);}
+public void setPostalCode(String postalCode){
+	elementPostalCode.sendKeys(postalCode);}
+	
 }
+
+	

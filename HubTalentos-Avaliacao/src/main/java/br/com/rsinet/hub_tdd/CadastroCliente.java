4ae335f036efa495002da6984d4905b3679ab35d
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-13 09:58:42 -0300

    Excluido o modo de espera Thread.sleep, e inserido o ImplicitlyWait

--- HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/CadastroCliente.java
+++ HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/CadastroCliente.java
@@ -1,60 +1,54 @@
 package br.com.rsinet.hub_tdd;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 
 public class CadastroCliente {
 
 	private WebDriver driver;
-	
 	
 
 	@Before
 	public void Inicializa() throws InterruptedException {
 		driver = new ChromeDriver();
 		driver.get("https://www.advantageonlineshopping.com/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
-		Thread.sleep(4000);
 	}
 
 	@Test
 	public void NovoUsuario() throws InterruptedException {
+		// Instancia á Página de Registro como uma PageFactory
+		PageRegister registerPage = PageFactory.initElements(driver, PageRegister.class);
 		
 		driver.findElement(By.id("menuUser")).click();
-		Thread.sleep(4000);
-		driver.findElement(By.xpath("/html/body/login-modal/div/div/div[3]/a[2]")).click();
-		Thread.sleep(4000);
-		
-		//Instancia á Página de Registro como uma PageFactory 
-		 PageRegister registerPage = PageFactory.initElements(driver, PageRegister.class);
-		
-		 
-		 
-		 
-		 registerPage.setUserName("Excel");
-		 registerPage.setEmai("Excel");
-		 registerPage.setPassword("Excel");
-		 registerPage.setConfirmPassword("Excel");
-		 
-		 registerPage.setFirstName("Excel");
-		 registerPage.setLastName("Excel");
-		 registerPage.setPhoneNumber("Excel");
-		 
-		 registerPage.selectCountry("Virgin Islands (USA)");
-		 registerPage.setCity("Excel");
-		 registerPage.setAdress("Excel");
-		 registerPage.setState("Excel");
-		 registerPage.setPostalCode("Excel");
-		
+		driver.findElement(By.xpath("/html/body/login-modal/div/div/div[3]/a[2]")).sendKeys(Keys.ENTER);
+
+		registerPage.setUserName("Excel");
+		registerPage.setEmai("Excel");
+		registerPage.setPassword("Excel");
+		registerPage.setConfirmPassword("Excel");
+
+		registerPage.setFirstName("Excel");
+		registerPage.setLastName("Excel");
+		registerPage.setPhoneNumber("Excel");
+
+		registerPage.selectCountry("Virgin Islands (USA)");
+		registerPage.setCity("Excel");
+		registerPage.setAdress("Excel");
+		registerPage.setState("Excel");
+		registerPage.setPostalCode("Excel");
+
+		registerPage.elementCheckReceiveOffersByEmail.click();
+		registerPage.elementCheckConditionsOfUse.click();
+		registerPage.elementButtonRegister.click();
 	}
 
 }

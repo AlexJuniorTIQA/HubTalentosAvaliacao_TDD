f849b63e4cc97506ed949354800460538f3dfe1e
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-13 09:57:19 -0300

    Excluido o metodo de espera, por não ser necessário.

--- HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/PageRegister.java
+++ HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/PageRegister.java
@@ -10,32 +10,31 @@ import org.openqa.selenium.support.ui.Select;
 public class PageRegister {
 
 	final WebDriver driver;
-	/////////////Elements: Account Details////////////////
+	///////////// Elements: Account Details////////////////
 	@FindBy(how = How.NAME, using = "usernameRegisterPage")
 
 	public WebElement elementUserName;
-	
+
 	@FindBy(how = How.NAME, using = "emailRegisterPage")
 
 	public WebElement elementEmail;
-	
-	
+
 	@FindBy(how = How.NAME, using = "passwordRegisterPage")
 
 	public WebElement elementPassword;
 	@FindBy(how = How.NAME, using = "confirm_passwordRegisterPage")
 
 	public WebElement elementConfirmPassword;
-	
-	/////////////Elements: Personal Details////////////////
+
+	///////////// Elements: Personal Details////////////////
 	@FindBy(how = How.NAME, using = "first_nameRegisterPage")
 
 	public WebElement elementFirstName;
-	
+
 	@FindBy(how = How.NAME, using = "last_nameRegisterPage")
 
 	public WebElement elementLastName;
-	
+
 	@FindBy(how = How.NAME, using = "phone_numberRegisterPage")
 
 	public WebElement elementPhoneNumber;
@@ -43,78 +42,102 @@ public class PageRegister {
 	@FindBy(how = How.NAME, using = "countryListboxRegisterPage")
 
 	public WebElement elementCountry;
-	
+
 	@FindBy(how = How.NAME, using = "cityRegisterPage")
 
 	public WebElement elementCity;
-	
+
 	@FindBy(how = How.NAME, using = "addressRegisterPage")
 
 	public WebElement elementAdress;
-	
+
 	@FindBy(how = How.NAME, using = "state_/_province_/_regionRegisterPage")
 
 	public WebElement elementState;
-	
+
 	@FindBy(how = How.NAME, using = "postal_codeRegisterPage")
 
 	public WebElement elementPostalCode;
 
-	
+/////////////Elements: Check Box////////////////
+
+	@FindBy(how = How.NAME, using = "allowOffersPromotion")
+
+	public WebElement elementCheckReceiveOffersByEmail;
+
+	@FindBy(how = How.NAME, using = "i_agree")
+
+	public WebElement elementCheckConditionsOfUse;
+
+	@FindBy(how = How.NAME, using = "register_btnundefined")
+
+	public WebElement elementButtonRegister;
+
 	////////////////////////////////////////////////////////
 	public PageRegister(WebDriver driver) {
 
 		this.driver = driver;
 	}
-	
-/**
- * Methods Account Details
- */
-	
-public void setUserName(String username){
-	elementUserName.sendKeys(username);}
 
-public void setEmai(String email){
-	elementEmail.sendKeys(email);}
+	/**
+	 * Methods Account Details
+	 */
 
-public void setPassword(String password){
-	elementPassword.sendKeys(password);}
+	public void setUserName(String username) {
+		elementUserName.sendKeys(username);
+	}
 
-public void setConfirmPassword(String password){
-	elementConfirmPassword.sendKeys(password);}
+	public void setEmai(String email) {
+		elementEmail.sendKeys(email);
+	}
 
-/**
- * Methods Personal Details
- * */
-public void setFirstName(String fistName){
-	elementFirstName.sendKeys(fistName);}
+	public void setPassword(String password) {
+		elementPassword.sendKeys(password);
+	}
 
-public void setLastName(String lastName){
-	elementLastName.sendKeys(lastName);}
+	public void setConfirmPassword(String password) {
+		elementConfirmPassword.sendKeys(password);
+	}
 
-public void setPhoneNumber(String phoneNumber){
-	elementPhoneNumber.sendKeys(phoneNumber);}
+	/**
+	 * Methods Personal Details
+	 */
+	public void setFirstName(String fistName) {
+		elementFirstName.sendKeys(fistName);
+	}
 
+	public void setLastName(String lastName) {
+		elementLastName.sendKeys(lastName);
+	}
 
-/**
- * Methods Account Details
- */
-public void selectCountry(String valor) {
-	WebElement element = driver.findElement(By.name("countryListboxRegisterPage"));
+	public void setPhoneNumber(String phoneNumber) {
+		elementPhoneNumber.sendKeys(phoneNumber);
+	}
+
+	/**
+	 * Methods Account Details
+	 */
+	public void selectCountry(String valor) {
+		WebElement element = driver.findElement(By.name("countryListboxRegisterPage"));
 		Select combo = new Select(element);
 		combo.selectByVisibleText(valor);
-		}
-public void setCity(String city){
-	elementCity.sendKeys(city);}
-		
-public void setAdress(String adress){
-	elementAdress.sendKeys(adress);}
-		
-public void setState(String state){
-	elementState.sendKeys(state);}
-public void setPostalCode(String postalCode){
-	elementPostalCode.sendKeys(postalCode);}
-	
-}
+	}
+
+	public void setCity(String city) {
+		elementCity.sendKeys(city);
+	}
+
+	public void setAdress(String adress) {
+		elementAdress.sendKeys(adress);
+	}
+
+	public void setState(String state) {
+		elementState.sendKeys(state);
+	}
+
+	public void setPostalCode(String postalCode) {
+		elementPostalCode.sendKeys(postalCode);
+	}
 
 	
+}

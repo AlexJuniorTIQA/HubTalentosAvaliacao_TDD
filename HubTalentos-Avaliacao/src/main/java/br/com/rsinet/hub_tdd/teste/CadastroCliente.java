17aa06f3588857c0204cbd3b772c9dd22b071af3
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-14 10:24:19 -0300

    Incluido o Assert Equals para validação

--- HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/teste/CadastroCliente.java
+++ HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/teste/CadastroCliente.java
@@ -1,24 +1,19 @@
 package br.com.rsinet.hub_tdd.teste;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestName;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import br.com.rsinet.hub_tdd.page.HomePage;
 import br.com.rsinet.hub_tdd.page.PageRegister;
 import br.com.rsinet.hub_tdd.teste.data.Constant;
 import br.com.rsinet.hub_tdd.teste.data.ExcelUtils;
@@ -31,27 +26,25 @@ public class CadastroCliente {
 
 	@Before
 	public void Inicializa() throws InterruptedException {
+		
 		driver = new ChromeDriver();
 		driver.get("https://www.advantageonlineshopping.com/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
-	@Rule
-	public TestName testName = new TestName();
 	@After
 	public void finaliza() throws IOException {
-		Screenshot.getScreenShot(driver);// verificar está devolvendo nulo, ver video udemy
+		Screenshot.getScreenShot(driver, "TestNewUser");
 	}
 
 	@Test
 	public void NovoUsuario() throws Exception {
+		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
 		PageRegister registerPage = PageFactory.initElements(driver, PageRegister.class);
         ExcelUtils.setExcelFile(Constant.File_TestData,"Planilha1");
 		
-		
-		driver.findElement(By.id("menuUser")).click();
-		driver.findElement(By.xpath("/html/body/login-modal/div/div/div[3]/a[2]")).sendKeys(Keys.ENTER);
-
+		homePage.elementIconeUser.click();
+		homePage.elementCreatNewAccount.sendKeys(Keys.ENTER);
 		
 		registerPage.setUserName(ExcelUtils.getCellData(1,1));
 		registerPage.setEmail(ExcelUtils.getCellData(1,2));
@@ -67,10 +60,14 @@ public class CadastroCliente {
 		registerPage.setAdress(ExcelUtils.getCellData(1,9));
 		registerPage.setState(ExcelUtils.getCellData(1,10));
 		registerPage.setPostalCode(ExcelUtils.getCellData(1,11));
-
+	
 		registerPage.elementCheckReceiveOffersByEmail.click();
 		registerPage.elementCheckConditionsOfUse.click();
 		registerPage.elementButtonRegister.click();
+		
+		
+		Thread.sleep(2000);
+		Assert.assertEquals(ExcelUtils.getCellData(1, 1), homePage.elementUserLink.getText());
 		
 	}
 	

637d09f118964ec9e9c6b39b11051f9b199bf3b4
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-14 13:10:58 -0300

    Alterado o nome da váriavel do arquivo excel

--- HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/teste/data/ExcelUtils.java
+++ HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/teste/data/ExcelUtils.java
@@ -22,7 +22,7 @@ public class ExcelUtils {
 
 	private static XSSFRow Row;
 
-//This method is to set the File path and to open the Excel file, Pass Excel Path and Sheetname as Arguments to this method
+
 
 	public static void setExcelFile(String Path, String SheetName) throws Exception {
 
@@ -32,7 +32,7 @@ public class ExcelUtils {
 
 			FileInputStream ExcelFile = new FileInputStream(Path);
 
-// Access the required test data sheet
+
 
 			ExcelWBook = new XSSFWorkbook(ExcelFile);
 
@@ -46,7 +46,7 @@ public class ExcelUtils {
 
 	}
 
-//This method is to read the test data from the Excel cell, in this we are passing parameters as Row num and Col num
+
 
 	public static String getCellData(int RowNum, int ColNum) throws Exception {
 
@@ -60,13 +60,13 @@ public class ExcelUtils {
 
 		} catch (Exception e) {
 
-			return "alex";
+			return "";
 
 		}
 
 	}
 
-//This method is to write in the Excel cell, Row num and Col num are the parameters
+
 
 	public static void setCellData(String Result, int RowNum, int ColNum) throws Exception {
 
@@ -90,7 +90,7 @@ public class ExcelUtils {
 
 			// Constant variables Test Data path and Test Data file name
 
-			FileOutputStream fileOut = new FileOutputStream(Constant.File_TestData);
+			FileOutputStream fileOut = new FileOutputStream(Constant.File_DataUserRegister);
 
 			ExcelWBook.write(fileOut);
 

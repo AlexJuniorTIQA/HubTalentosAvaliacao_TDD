24f8a7aeb044be9438d4b5320488f3163c251f31
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-14 13:11:27 -0300

    Alterado o nome da variavel do arquivo Excel

--- HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/teste/data/Constant.java
+++ HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/teste/data/Constant.java
@@ -1,5 +1,5 @@
 package br.com.rsinet.hub_tdd.teste.data;
 public class Constant {
-public static final String File_TestData = "AdvantageOnline_DataPageRegister.xlsx";
+public static final String File_DataUserRegister = "AdvantageOnline_Data.xlsx";
 
 }

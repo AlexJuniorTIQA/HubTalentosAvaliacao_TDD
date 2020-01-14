e82641e4641776ff018c29179db9e78725a1e0e1
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-14 10:25:12 -0300

    Incluida á opção de definir o nome do teste para a screenshot

--- HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/teste/data/Screenshot.java
+++ HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/teste/data/Screenshot.java
@@ -2,33 +2,27 @@ package br.com.rsinet.hub_tdd.teste.data;
 
 
 	import java.io.File;
-	import java.io.IOException;
-	import java.text.SimpleDateFormat;
-	import java.util.Calendar;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
-
-	import org.apache.commons.io.FileUtils;
-import org.junit.Rule;
-import org.junit.rules.TestName;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
-	import org.openqa.selenium.TakesScreenshot;
-	import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
 
 	public class Screenshot {
-	    
-		@Rule
-		public static TestName testName = new TestName();
-		
+
 	    public static String getTimeStamp() {
 	        return new SimpleDateFormat("dd_MM_yyyy HH.mm.ss").format(Calendar.getInstance().getTime());
 	    }
 	    
-	    public static void getScreenShot(WebDriver driver){
+	    public static void getScreenShot(WebDriver driver, String testName){
 	        File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 	        
 	        try {
-	            FileUtils.copyFile(file, new File("target/screenshot/"+ testName.getMethodName()+ getTimeStamp() + ".png"));
+	            FileUtils.copyFile(file, new File("target/screenshot/"+ testName+ " " + getTimeStamp() + ".png"));
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }

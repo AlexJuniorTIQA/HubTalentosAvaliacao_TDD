aca8d8f97a4eb863c744525905039f5aafa1e201
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-15 14:55:59 -0300

    Sem alterações válidas

---
+++ HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/files/Constant.java
@@ -0,0 +1,5 @@
+package br.com.rsinet.hub_tdd.files;
+public class Constant {
+public static final String File_DataUserRegister = "AdvantageOnline_Data.xlsx";
+
+}
---
+++ HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/teste/ConsultaProdutoBarraDePesquisa.java
@@ -0,0 +1,60 @@
+package br.com.rsinet.hub_tdd.teste;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import br.com.rsinet.hub_tdd.files.Constant;
+import br.com.rsinet.hub_tdd.files.ExcelUtils;
+import br.com.rsinet.hub_tdd.files.Screenshot;
+import br.com.rsinet.hub_tdd.page.HomePage;
+import br.com.rsinet.hub_tdd.page.ProductPage;
+
+public class ConsultaProdutoBarraDePesquisa {
+
+	private WebDriver driver;
+	
+
+	@Before
+	public void Inicializa() throws InterruptedException {
+		driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1336, 768));
+		driver.get("https://www.advantageonlineshopping.com/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+	}
+	@After
+	public void finaliza() throws IOException {
+	driver.quit();
+	}
+	@Test
+	public void ConsultarProduto ()throws Exception  {
+		
+		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+		ProductPage productPage = PageFactory.initElements(driver, ProductPage.class);
+		ExcelUtils.setExcelFile(Constant.File_DataUserRegister,"Headphone");
+			
+		homePage.javaScriptClick(By.id("miceTxt")); 
+		homePage.setSearch(ExcelUtils.getCellData(2, 1) + Keys.ENTER);
+		homePage.findElementLinkText(ExcelUtils.getCellData(2, 1)); 
+		
+		productPage.assertEqualsProduct(ExcelUtils.getCellData(2,1));
+		
+		Screenshot.getScreenShot(driver, "TesteConsultaProduto ");
+		
+		
+		
+	}
+}

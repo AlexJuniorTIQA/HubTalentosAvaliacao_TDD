70a9d9741754c16255ae5247a585f64e023efe46
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-16 12:10:40 -0300

    Classe criada á parametrizar o manuseio do driver de navegador.

---
+++ HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/page/DriverElement.java
@@ -0,0 +1,20 @@
+package br.com.rsinet.hub_tdd.page;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverElement {
+	WebDriver driver;
+
+	public WebDriver getChromeDriver(WebDriver driver) {
+		driver = new ChromeDriver();
+		driver.get("https://www.advantageonlineshopping.com/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().setSize(new Dimension(1200, 800));
+		return driver;
+
+	}
+}

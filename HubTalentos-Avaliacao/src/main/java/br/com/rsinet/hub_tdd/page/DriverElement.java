7a05aba3b3d50ecae06092c3fdcb8b4ad3578163
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-17 09:26:49 -0300

    Foi corrigido o retorno quit do driver, para static void, recebendo o driver.
    E o getChromeDriver sem receber nenhum parametro, pois assim o mesmo
    daria NullPointerException

--- HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/page/DriverElement.java
+++ HubTalentos-Avaliacao/src/main/java/br/com/rsinet/hub_tdd/page/DriverElement.java
@@ -7,14 +7,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverElement {
-	WebDriver driver;
+	static WebDriver driver;
 
-	public WebDriver getChromeDriver(WebDriver driver) {
+	public static WebDriver getChromeDriver(){
+
 		driver = new ChromeDriver();
 		driver.get("https://www.advantageonlineshopping.com/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().setSize(new Dimension(1200, 800));
 		return driver;
-
+	}
+	
+	public static void quitDriver(WebDriver driver) {
+		if(driver != null)
+			driver.quit();
 	}
 }
